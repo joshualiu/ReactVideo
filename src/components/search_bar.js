6f8c697b7254917d3d81f4186336afe0d918da64
zhonghaoliu <joshualiu94@gmail.com>
2018-10-20 18:44:00 -0400

    ajax request with react

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,26 +1,28 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: 'Hey' };
-  }
-
-  onInputChange(event) {
-    console.log(event.target.value)
+    this.state = { term: '' };
   }
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input 
-          value={this.state.term}
-          onChange={ event => this.setState({ term: event.target.value }) }
+          value={ this.state.term }
+          onChange={ event => this.onInputChange(event.target.value) }
           />
       </div>
     )
   }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSeachTermChange(term);
+  }
+
   
 }
 
